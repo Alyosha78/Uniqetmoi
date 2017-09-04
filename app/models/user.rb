5d1f2b95a81baae6565f24acd5cdc35be1df322c
Alyosha MIRABILE <mirabile.alyosha@gmail.com>
2017-09-04 09:09:30 +0000

    création du champ fullname

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+     validate :fullname, presence:true, lenght: {maximum: 50}
+     
 end
